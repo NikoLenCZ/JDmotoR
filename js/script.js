7494dfca560e14e37f8967f8b1cc32036a507389
LenyNik <55710950+LenyNik@users.noreply.github.com>
2021-04-13 11:36:26 +0200

    zaloha

---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+    // Menu-toggle button
+
+    $(document).ready(function () {
+
+      $(".menu-icon").on("click", function () {
+        $("nav ul").toggleClass("showing"),
+        $( "#menu").toggleClass( "rotate");
+      });
+    });
+
+    $(".menu a").on("click", function () {
+      $("nav ul").toggleClass("showing"),
+        $( "#menu").toggleClass( "rotate");
+    });
+
+
+
+    // hustej efekt rotate menu
+  //   $( ".menu-icon" ).click(function() {
+  //     $( this ).toggleClass( "rotate");
+  // });
+
+//   $( ".menu-icon" ).on("click", function() {
+//     $( this ).toggleClass( "rotate");
+// });
+
+// $( ".menu-icon" ).on("click", function() {
+//   $( "i" ).toggleClass( "rotate");
+// });
+
+
+    // Scrolling Effect
+
+    $(window).on("scroll", function () {
+      if ($(window).scrollTop()) {
+        $('nav').addClass('blue');
+      } else {
+        $('nav').removeClass('blue');
+      }
+    })
+
+
+    /*
+	 * LIGHTBOX
+	 */
+	
+	
